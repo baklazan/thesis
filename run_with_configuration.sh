a0a5c8a493606939e4fa41a1a09c5d4354a7a20f
Eduard Batmendijn <eduard.batmendijn@innovatrics.com>
2018-05-10 02:46:35 +0200

    bugfix

--- run_with_configuration.sh
+++ run_with_configuration.sh
@@ -7,9 +7,9 @@ OUTPUT_DIR=$3
 
 set -e
 mkdir $OUTPUT_DIR
-
+mkdir $OUTPUT_DIR/results
 
 for dir in $DATASET/reads/*
 do
-  python3 detect_SNP.py $dir/reference.fasta $dir -i -o $OUTPUT_DIR/$(basename $dir).txt -c $CONFIG
+  python3 detect_SNP.py $dir/reference.fasta $dir -i -o $OUTPUT_DIR/results/$(basename $dir).txt -c $CONFIG
 done
