12ea4ded5fe05f259e5fcc323f2e401b354ef1c8
Eduard Batmendijn <eduard.batmendijn@innovatrics.com>
2018-05-11 22:26:59 +0200

    Bugfix and progressbar in data preparation script.

--- prepare_dataset.sh
+++ prepare_dataset.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-#parameters: read_basedir, reference_fasta, result_dir, SNP_percentage
+
+if [ "$#" -ne 4 ]; then
+    echo "usage: $0 read_basedir reference_fasta result_dir SNP_percentage"
+    exit 1
+fi
 
 READ_BASEDIR=$1
 REFERENCE_FASTA=$2
@@ -9,19 +13,25 @@ SNP_PERCENTAGE=$4
 set -e
 mkdir $RESULT_DIR
 
+echo "[prepare]: initial reference indexing..."
 REFERENCE_COPY=$RESULT_DIR/original_reference.fasta
 cp $REFERENCE_FASTA $REFERENCE_COPY
-bwa index $REFERENCE_COPY 2> /dev/null
+bwa index $REFERENCE_COPY #2> /dev/null
+
+echo "[prepare]: copying reads..."
 
 for read in $READ_BASEDIR/*.fast5
 do
   cp $read $RESULT_DIR/$(basename $read)
 done
 
+echo "[prepare]: initial resquiggle..."
+tombo resquiggle $RESULT_DIR $REFERENCE_COPY --overwrite --bwa-mem-executable bwa #--quiet 2> /dev/null
 
-tombo resquiggle $RESULT_DIR $REFERENCE_COPY --overwrite --bwa-mem-executable bwa --quiet 2> /dev/null
+echo "[prepare]: preparing directory for reads..."
 mkdir $RESULT_DIR/reads
 
+echo "[prepare]: preparing subdirectories for reads..."
 for read in $RESULT_DIR/*.fast5
 do
   filename=`echo "$read" | sed -r "s/.+\/(.+)\..+/\1/"`
@@ -29,10 +39,18 @@ do
   mv $read $RESULT_DIR/reads/$filename/read.fast5
 done
 
+echo "[prepare]: making artificial SNPs..."
 python3 make_SNPs.py $RESULT_DIR/reads $REFERENCE_COPY $SNP_PERCENTAGE
 
+echo "[prepare]: indexing and resquiggling to changed reference..."
+CURRENT_READ=0
+NUMBER_OF_READS=`ls $RESULT_DIR/reads -1 | wc -l`
 for read_dir in $RESULT_DIR/reads/*
 do
+  CURRENT_READ=`expr $CURRENT_READ + 1`
+  echo "processing read $CURRENT_READ / $NUMBER_OF_READS"
+  set +e
   bwa index $read_dir/reference.fasta 2> /dev/null
+  set -e
   tombo resquiggle $read_dir $read_dir/reference.fasta --overwrite --bwa-mem-executable bwa --quiet 2> /dev/null
 done
