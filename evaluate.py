c7d12dd3d652f2e982e5470118305c1f73b4609b
Eduard Batmendijn <eduard.batmendijn@innovatrics.com>
2018-05-16 14:45:06 +0200

    Changes in plot script.

--- evaluate.py
+++ evaluate.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib.colors import to_rgba
 import numpy as np
 import argparse
 from alphabet import *
@@ -14,6 +15,9 @@ parser.add_argument("dataset", help="dataset directory")
 parser.add_argument("results", help="directories with experiment results", nargs="+")
 parser.add_argument("-l", "--labels", help="labels for result files, must come after results", nargs="+")
 parser.add_argument("-o", "--output", help="also save plot to file")
+parser.add_argument("-x", "--xlabel", help="label for x axis")
+parser.add_argument("-s", "--suptitle", help="suptitle")
+parser.add_argument("-z", "--zoom", help="part of ROC curve to be ploted")
 args = parser.parse_args()
 
 if args.labels and len(args.labels) != len(args.results):
@@ -47,6 +51,8 @@ def intersection(lists):
     indices[mini_id] += 1
   return result
 
+line_style_cycle = ['-', '--', '-.', ':']
+line_style_idx = 0
 def plot_rocs(scores, label):
   scores = list(chain.from_iterable(scores))
   scores.sort(reverse=True)
@@ -77,93 +83,93 @@ def plot_rocs(scores, label):
       best_tpr = tpr
   x.append(1.0)
   y.append(1.0)
+  global line_style_idx
   if len(x) > 100:
-    plt.plot(x, y, label=label)
+    plt.plot(x, y, label=label, linestyle=line_style_cycle[line_style_idx])
   else:
-    plt.plot(x, y, linewidth=0.2, markersize=2, marker='o', label=label)
+    plt.plot(x, y, linewidth=0.2, markersize=2, marker='o', label=label, linestyle=line_style_cycle[line_style_idx])
+  line_style_idx += 1
+  line_style_idx %= len(line_style_cycle)
 
 def decorate_rocs():
-  plt.suptitle("ROC")
-  plt.xlabel("False positive rate")
-  plt.ylabel("True positive rate")
-  plt.xlim((0, 1))
+  plt.suptitle(args.suptitle if args.suptitle else "ROC")
+  plt.xlabel(args.xlabel if args.xlabel else "FP")
+  plt.ylabel("TP")
+  plt.xlim((0, float(args.zoom) if args.zoom else 1))
   plt.ylim((0, 1))
   plt.legend(loc='lower right')
 
-def histogram(values, bins):
-  step = 1 / bins
-  hist = np.zeros(bins)
-  edges = [i*step for i in range(bins+1)]
-  for v in values:
-    hist[max(0, min(bins-1, int(v / step)))] += 1
-  return hist, edges
 
-def plot_distribution(values, label):
-  hist, edges = histogram(values, bins=max(100, int(np.sqrt(len(values)))))
-  hist = hist / len(values)
-  for i, val in enumerate(hist):
-    hist[i] = val / (edges[i+1] - edges[i])
-  y = [0, 0, hist[0]]
-  x = [0, edges[0], edges[0]]
-  for i, val in enumerate(hist):
-    y.append(hist[i])
-    x.append((edges[i+1] + edges[i])/2)
-  y.append(hist[-1])
-  x.append(edges[-1])
-  y.append(0)
-  x.append(edges[-1])
-  y.append(0)
-  x.append(1)
-  plt.plot(x, y, label=label)
 
+def plot_distribution(values, label, log=False, bins='auto', normed=False):
+  color = next(plt.gca()._get_lines.prop_cycler)['color']
+  plt.hist(values, label=label, color=to_rgba(color, 0.5), bins=bins, log=log, normed=normed)
 
 def plot_score_dist(scores, label):
   scores = list(chain.from_iterable(scores))
   positive = [score for score, type in scores if type == 0]
   negative = [score for score, type in scores if type > 0]
-  plot_distribution(positive, "SNP")
-  plot_distribution(negative, "nonSNP")
+  print('SNPs: {}'.format(len(positive)))
+  print('nonSNPs: {}'.format(len(negative)))
+  bins=int(min(len(positive), len(negative)) ** (1/3))
+  plot_distribution(positive, "SNP", log=True, bins=bins, normed=True)
+  plot_distribution(negative, "bez SNPu", log=True, bins=bins, normed=True)
 
 def plot_proximity_to_score_dist(scores, label, classes):
-  plt.xlim((0, 1))
   scores = list(chain.from_iterable(scores))
+  scs = []
+  minlen = float('inf')
   for c in classes:
     sc = [score for score, dist in filter(lambda x: x[1] >= c[0] and x[1] < c[1], scores)]
-    plot_distribution(sc, c[2])
+    scs.append(sc)
+    minlen = min(minlen, len(sc))
 
-def plot_top_X_success(all_scores, label):
+  bins = int(minlen ** (1/3))
+  for i, c in enumerate(classes):
+    sc = scs[i]
+    print('{}: {}'.format(c, len(sc)))
+    plot_distribution(sc, c[2], log=True, bins=bins, normed=True)
+
+def plot_top_X_success(all_scores, label, bins='auto'):
   ratios = []
+  found_SNP_sum = 0
+  SNP_count_sum = 0
   for scores in all_scores:
     scores.sort(reverse=True, key=lambda x: (x[0], random.random()))
     SNP_count = len([None for x in scores if x[1] == 0])
     found_SNP_count = len([None for x in scores[:SNP_count] if x[1] == 0])
+    SNP_count_sum += SNP_count
+    found_SNP_sum += found_SNP_count
     if SNP_count > 0:
       ratios.append(found_SNP_count / SNP_count)
-  plot_distribution(ratios, label)
+  plot_distribution(ratios, '{} ({:0.1f} %)'.format(label, found_SNP_sum/SNP_count_sum*100), bins=bins)
+  print('{}: {} %'.format(label, found_SNP_sum/SNP_count_sum*100))
 
 
 
 def decorate_top_X_success():
-  plt.suptitle("Identification success distribution")
+  plt.suptitle(args.suptitle if args.suptitle else "Distribúcia úspešnosti identifikácie")
   plt.xlim((0, 1))
   plt.ylim(ymin=0)
-  plt.xlabel("score")
-  plt.ylabel("normalized frequency")
-  plt.legend(loc='upper right')
+  plt.xlabel(args.xlabel if args.xlabel else "skóre")
+  plt.ylabel("početnosť")
+  plt.legend(loc='lower right')
 
 
 def decorate_score_dist():
-  plt.suptitle("Score distribution")
+  plt.suptitle(args.suptitle if args.suptitle else "Distribúcia skóre")
   plt.xlim((0, 1))
-  plt.ylim(ymin=0)
-  plt.xlabel("score")
-  plt.ylabel("normalized frequency")
+  plt.xlabel(args.xlabel if args.xlabel else "skóre")
+  plt.ylabel("normalizovaná početnosť")
   plt.legend(loc='upper center')
 
 
 outfiles = []
 for experiment in args.results:
-  outfiles.append(list(filter(lambda x: x[-4:] == '.txt', os.listdir(experiment))))
+  outfiles.append(list(filter(lambda x: x[-4:] == '.txt' and os.stat(os.path.join(experiment, x)).st_size > 0, os.listdir(experiment))))
+  emptyfiles = list(filter(lambda x: x[-4:] == '.txt' and os.stat(os.path.join(experiment, x)).st_size == 0, os.listdir(experiment)))
+  print('{}: {} files are empty'.format(experiment, len(emptyfiles)))
+
 
 outfiles = intersection(outfiles)
 
@@ -222,10 +228,11 @@ elif args.plot_type == 'score_dist':
   plot_function = plot_score_dist
   decoration_function = decorate_score_dist
 elif args.plot_type == 'proximity':
-  plot_function = lambda x, y: plot_proximity_to_score_dist(x, y, ((0, 1, "SNP"), (1, 2, "next to SNP"), (10, float('inf'), "≥10 bases from SNP"),))
+  plot_function = lambda x, y: plot_proximity_to_score_dist(x, y, ((0, 1, "SNP"), (1, 2, "vedľa SNPu"), (10, float('inf'), "≥10 báz od SNPu"),))
   decoration_function = decorate_score_dist
 elif args.plot_type == 'top':
-  plot_function = plot_top_X_success
+  print(min([len(e[0]) for e in experiments]))
+  plot_function = lambda x, y: plot_top_X_success(x, y, bins=[i*0.04 for i in range(26)])
   decoration_function = decorate_top_X_success
 else:
   print('unknown metric: {}'.format(args.plot_type))
